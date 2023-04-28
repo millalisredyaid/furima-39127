23d5e0e76bb3206305a1490dc3ae47213fbc290d
millalisredyaid <milanaxrazdukhova@gmail.com>
2023-04-28 21:17:06 +0900

    削除、修正、テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,17 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     last_name { "赤西" }
     first_name { "未来" }
     last_name_kana { "アカニシ" }
     first_name_kana { "ミライ" }
     birth_date { "1990-01-01" }
+    
+    trait :invalid_password do
+      password { '123456' }
+      password_confirmation { '123456' }
+    end 
   end
 end
