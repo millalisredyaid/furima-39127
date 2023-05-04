7536608623d7b90a3e3de94cf0e13463f97bdf99
millalisredyaid <milanaxrazdukhova@gmail.com>
2023-05-04 17:18:11 +0900

    修正と削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe Item, type: :model do
     end
     context '商品が投稿できない場合' do  
       it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
       it 'item_nameが空だと登録できない' do
         @item.item_name = nil
